0add97c0b3ba9ccf6178132fb3150495a2063817
Aidas Vaiciunas <vaiciunasaidas@gmail.com>
2024-05-19 18:34:19 +0100

    deploy fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-netlify";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
